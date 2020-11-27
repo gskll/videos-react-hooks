df70e1676d2d2a9758d7bd93bc4ed328eb443978
Andrew <gskll@protonmail.com>
2020-11-27 18:57:38 +0100

    refactor: remove unnecessary video select callback
    - previously refactored to use useState setter
    - can reference directly in jsx tag

--- src/components/App.js
+++ src/components/App.js
@@ -24,10 +24,6 @@ const App = () => {
     setSelectedVideo(response.data.items[0]);
   };
 
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
   return (
     <div className="ui container">
       <SearchBar onTermSubmit={onTermSubmit} />
@@ -37,7 +33,7 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
